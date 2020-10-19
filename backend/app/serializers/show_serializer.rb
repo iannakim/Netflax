97c0eb3f4d8e09007f3e51f9cb81bb099fdc1071
iannakim <dear.hyunji@gmail.com>
2020-10-19 14:39:49 -0400

    serializer

--- backend/app/serializers/show_serializer.rb
+++ backend/app/serializers/show_serializer.rb
@@ -1,3 +1,6 @@
 class ShowSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :image, :title, :year, :maturity, :runtime, :description, :casts, :creators, :history
+  belongs_to :type
+  belongs_to :genre
+  has_many :reviews
 end
